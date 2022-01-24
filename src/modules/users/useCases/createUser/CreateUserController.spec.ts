21d0ac81755a55ce5e47e8fbde8637758bb520e8
Rafael Miranda <rafaelmdurante@gmail.com>
2022-01-24 13:26:26 -0300

    test(users): add integration tests for post /users endpoint

--- src/modules/users/useCases/createUser/CreateUserController.spec.ts
+++ src/modules/users/useCases/createUser/CreateUserController.spec.ts
@@ -1,11 +1,15 @@
-import { CreateUserController } from "./CreateUserController"
 import { app } from '../../../../app'
 import createConnection from '../../../../database/index'
 import { Connection } from "typeorm"
 import request from 'supertest'
 
 let connection: Connection
-let endpoint = '/api/v1/users'
+let usersEndpoint = '/api/v1/users'
+let mockUser = {
+    name: 'fake user',
+    email: 'fake@mail.com',
+    password: 'fake password'
+}
 
 describe('POST /users', () => {
     beforeAll(async () => {
@@ -20,14 +24,21 @@ describe('POST /users', () => {
 
     it('should be able to create a user', async () => {
         const response = await request(app)
-            .post(endpoint)
-            .send({
-                name: 'fake user',
-                email: 'fake@mail.com',
-                password: 'fake password'
-            })
+            .post(usersEndpoint)
+            .send(mockUser)
 
         expect(response).toHaveProperty('status', 201)
-        expect(response).toHaveProperty('body')
+    })
+
+    it('should not be able to create two users with the same email', async () => {
+        await request(app).post(usersEndpoint).send(mockUser)
+
+        const response = await request(app).post(usersEndpoint).send({
+            email: mockUser.email,
+            name: 'another fake user',
+            password: 'another_fake_password'
+        })
+
+        expect(response).toHaveProperty('status', 400)
     })
 })
